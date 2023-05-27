ddf15fd7e3a48771f989ec4e96f4e2c6a75e7855
dev-hobbyist <dev.hobbyist.777@gmail.com>
2023-05-27 20:22:01 +0900

    lib cdn

--- cms/web_server.js
+++ cms/web_server.js
@@ -4,13 +4,13 @@ const api = require('./api.js');
 var cors = require('cors');
 
 const blog_site = express();
-http.createServer(blog_site).listen(1080);
+http.createServer(blog_site).listen(2080);
 blog_site.use(express.static('../docs/'));
 
 const cms_site = express();
 cms_site.use(express.json({ limit: '50mb' }));
 cms_site.use(cors());
 
-http.createServer(cms_site).listen(1081);
+http.createServer(cms_site).listen(2081);
 cms_site.use(express.static('./docs'));
 cms_site.use('/api', api);
